d51d9740f12c5fe3a27e365b61955e125027a483
Rocco Sangellino <rsangellino@gmail.com>
2016-10-23 22:37:42 -0600

    Implemented the gaming logic

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 (function(){
 	'user strict';
-	
-	var gamePieces = document.querySelectorAll(".game-piece");
-	var level = 2;
 
-	var matchElements = [];
-	attachEvents();
+	var rootElement = document.getElementById("root");
+	var gamePieces = rootElement.querySelectorAll(".game-piece");
+	var levelContainer = rootElement.querySelectorAll(".level")[0];
+
+	if (!gamePieces.length) {
+	    alert("Simon Says is currently unavailable!");
+			return;
+	}
+
+	var level = 1;
+	var levelCounter = level;
+	var pattern = [];
+	var pieceClickIndex = 0;
+
 	createPattern();
+	attachEvents();
+
 	function createPattern() {
 		setTimeout(function() {
-			lightUpPiece();
-			if(--level) {
+			var piece = gamePieces[getRandomIndex()];
+			lightUpPiece(piece);
+			pattern.push(piece);
+			if(--levelCounter) {
 				createPattern();
 			}
 		}, 700);
@@ -21,24 +34,29 @@
 		for (var i = 0; i < gamePieces.length; i++) {
 			gamePieces[i].addEventListener('click', handleGamePiece, false);
 		}
-	}
 
+		rootElement.querySelectorAll(".start-over")[0].addEventListener('click', startOver, false);
+		rootElement.querySelectorAll(".level-up")[0].addEventListener('click', levelUp, false);
+
+	}
 
 	function handleGamePiece(e) {
 		var target = e.target;
 
-		if(target === matchElements[clickCount]) {
-			console.log("match!");
-		} else {
-			level = 1;
-			createPattern();
+		// the correcte pattern was clicked
+		if(target === pattern[pieceClickIndex]) {
+				lightUpPiece(target);
+				pieceClickIndex++;
+
+				// go to the next level
+				if(pieceClickIndex === level) {
+					levelUp();
+				}
 		}
-
-		setActiveGamePiece(target);
-
-		setTimeout(function() { 
-			removeActiveGamePiece(target);
-		}, 500);
+		// incorrect pattern start the game over
+		else {
+			startOver();
+		}
 	}
 
 	function setActiveGamePiece(element) {
@@ -46,23 +64,13 @@
 	}
 
 	function removeActiveGamePiece(element) {
-		element.classList.remove("active"); 
+		element.classList.remove("active");
 	}
 
-	function lightUpPiece() {
-		var pattern = [];
-		//for (var i = 0; i < level; i++) {
-			var index = getRandomIndex();
-			var piece = gamePieces[index];
-			matchElements.push(piece);
-			setAndRemovePiece(piece);
-		//}
-	}
-
-	function setAndRemovePiece(element) {
+	function lightUpPiece(element) {
 		setActiveGamePiece(element);
 
-		setTimeout(function() { 
+		setTimeout(function() {
 			removeActiveGamePiece(element);
 		}, 500);
 	}
@@ -71,16 +79,32 @@
 		return Math.floor((Math.random() * 4));
 	}
 
-	function start() {
-		var countDown = 3;
-		var c = setInterval(function() { 
-			console.log(countDown);
-			if(countDown === 0) {
-				setActiveGamePiece(gamePieces[rand]);
-				clearTimeout(c);
-			}
-			countDown--;
-		}, 1000);
+	function startOver() {
+		//reset settings to default state
+		level = 1;
+		setDefaults();
+		updateLevelContainer();
+		createPattern();
+	}
+
+	function levelUp() {
+		//reset settings but level up
+		level++;
+		setDefaults();
+		updateLevelContainer();
+		createPattern();
+	}
+
+	function updateLevelContainer() {
+		if(levelContainer) {
+			levelContainer.innerHTML = level;
+		}
+	}
+
+	function setDefaults() {
+		pattern = [];
+		levelCounter = level;
+		pieceClickIndex = 0;
 	}
 
 })();
